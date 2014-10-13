1191c4b36867fdd630dca4024a92f381ba319050
desigenerror <desigenerror@yandex.ru>
2014-10-13 21:53:00 +0400

    Adding omniauth for google

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,14 +1,24 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, 'sB0HcWW4k9E61fsxEm8rCzVUQ', 'TFbYE4o6aLWaQx9Wju9hSeZqvfFIloPQ4w76P2JtFWKBxxbSan'
-  provider :vkontakte, '4588251', 'kEADLqi95A18LrMT005N', {
+  provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
+  provider :vkontakte, ENV['VKONTAKTE_KEY'], ENV['VKONTAKTE_SECRET'], {
            redirect_uri: "https://0.0.0.0:3000/auth/vkontakte/callback"
   }
 
-  provider :facebook, '933244226703618', 'f05d73cf2024fed9714e9b94f09f896c'
-  provider :instagram, '', ''
-  provider :yandex, 'd95e615653f4404b9d829ca9505d3dc5', 'e1284bd720064015baba6ebf2072f461'
-  provider :mailru, '725720', 'a0f14f0c75c53aacd0a3b55bed3e72bf', {
-      #:callback_url => ENV['MAILRU_CALLBACK_URL']
-      :redirect_uri => "http://0.0.0.0:3000/auth/mailru/callback"
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+  provider :instagram, ENV['INSTAGRAM_KEY'], ENV['INSTAGRAM_SECRET'], {
+      redirect_uri: "https://0.0.0.0:3000/auth/instagram/callback"
+  }
+  provider :yandex, ENV['YANDEX_KEY'], ENV['YANDEX_SECRET']
+  provider :mailru, ENV['MAILRU_KEY'], ENV['MAILRU_SECRET'], {
+      redirect_uri: "https://0.0.0.0:3000/auth/mailru/callback",
+      response_type: 'token'
+  }
+
+  provider :google_oauth2, ENV['GMAIL_KEY'], ENV['GMAIL_SECRET'], {
+      :name => "google",
+      :scope => "email, profile, plus.me, http://gdata.youtube.com",
+      :prompt => "select_account",
+      :image_aspect_ratio => "square",
+      :image_size => 50
   }
 end

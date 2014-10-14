5bb739ee74e8a2a5f1da31b0b7b2d83874f76138
desigenerror <desigenerror@yandex.ru>
2014-10-14 19:33:47 +0400

    Changing oauth settings

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -10,14 +10,14 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :yandex, ENV['YANDEX_KEY'], ENV['YANDEX_SECRET']
   provider :mailru, ENV['MAILRU_KEY'], ENV['MAILRU_SECRET'], {
       redirect_uri: "http://mysterious-stream-6275.herokuapp.com/auth/mailru/callback",
-      response_type: 'token'
+      #response_type: 'token'
   }
 
   provider :google_oauth2, ENV['GMAIL_KEY'], ENV['GMAIL_SECRET'], {
-      :name => "google",
-      :scope => "email, profile, plus.me, http://gdata.youtube.com",
-      :prompt => "select_account",
-      :image_aspect_ratio => "square",
-      :image_size => 50
+      #:name => "google",
+      #:scope => "email, profile, plus.me, http://gdata.youtube.com",
+      #:prompt => "select_account",
+      #:image_aspect_ratio => "square",
+      #:image_size => 50
   }
 end

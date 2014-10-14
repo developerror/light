35cddf14f220fd99e1eb17ef05a6bf6ee6ab1784
desigenerror <desigenerror@yandex.ru>
2014-10-14 20:18:49 +0400

    Changing Google setting

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -13,11 +13,12 @@ Rails.application.config.middleware.use OmniAuth::Builder do
       #response_type: 'token'
   }
 
-  provider :google_oauth2, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET']#, {
+  provider :google_oauth2, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET'], {
+  	redirect_uri: "http://mysterious-stream-6275.herokuapp.com/auth/google_oauth2/callback"
       #:name => "google",
       #:scope => "email, profile, plus.me, http://gdata.youtube.com",
       #:prompt => "select_account",
       #:image_aspect_ratio => "square",
       #:image_size => 50
-  #}
+  }
 end
